acaf3922d0955d24b343da1a462f6e54fd92b9a6
John Wang <john.wang@gmail.com>
2011-01-25 21:43:59 -0800

    implemented the servlet and some html/js work

--- README.md
+++ README.md
@@ -14,4 +14,16 @@ url --> meaningfulweb engine --> structured information
 
 Build and release are managed via Maven: http://maven.apache.org/
 
-mvn package
+1. run the script: bin/mvn-install.sh to install .jar files in jars/ to local maven repo
+2. build core: under meaningfulweb-core/, do: mvn install
+3. start webapp: under meaningfulweb-app/, do: mvn jetty:run
+
+application should be running at: http://localhost:8080/meaningfulweb-app/
+
+the rest service should be running at: http://localhost:8080/meaningfulweb-app/get-meaning?url=xxx
+
+Example:
+
+http://localhost:8080/meaningfulweb-app/get-meaning?url=http://www.google.com
+
+
--- meaningfulweb-app/src/main/java/org/meaningfulweb/servlet/MeaningfulWebServlet.java
+++ meaningfulweb-app/src/main/java/org/meaningfulweb/servlet/MeaningfulWebServlet.java
@@ -3,24 +3,35 @@ package org.meaningfulweb.servlet;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.charset.Charset;
+import java.util.Map;
+import java.util.Set;
+import java.util.Map.Entry;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.tika.metadata.Metadata;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.meaningfulweb.api.MetaContentExtractor;
+
+import proj.og4j.OGObject;
 
 public class MeaningfulWebServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
 	private static Charset utf8 = Charset.forName("UTF-8");
+	private HttpClient httpClient = new HttpClient();
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 	                        throws ServletException, IOException {
 	  try {
-		
-		JSONObject resObj = extractContent();
+		String url = req.getParameter("url");
+		JSONObject resObj = extractContent(url);
         resp.setContentType("text/plain; charset=utf-8");
 		resp.setCharacterEncoding("UTF-8");
 		
@@ -32,9 +43,54 @@ public class MeaningfulWebServlet extends HttpServlet {
 	  }
 	}
 	
-	private JSONObject extractContent() throws JSONException{
+	private static JSONObject convert(Map<String,String> map,JSONObject obj) throws JSONException{
+		if (obj == null){
+			obj = new JSONObject();
+		}
+		
+		Set<Entry<String,String>> entries = map.entrySet();
+		for (Entry<String,String> entry : entries){
+			String name = entry.getKey();
+			String val = entry.getValue();
+			if (name!=null && val!=null){
+				obj.put(name, val);
+			}
+		}
+		
+		return obj;
+	}
+	
+	private JSONObject extractContent(String url) throws Exception{
+		GetMethod get = null;
 		JSONObject resObj = new JSONObject();
-		resObj.put("title", "faketitle");
-		return resObj;
+		try{
+			MetaContentExtractor extractor = new MetaContentExtractor();
+			get = new GetMethod(url);
+			httpClient.executeMethod(get);
+			
+			Metadata metadata = new Metadata();
+			metadata.add(Metadata.RESOURCE_NAME_KEY, url);
+			metadata.add(Metadata.CONTENT_TYPE, get.getResponseHeader(Metadata.CONTENT_TYPE).getValue());
+			OGObject obj = extractor.extract(url, get.getResponseBodyAsStream(), metadata,get.getResponseCharSet());
+			
+			resObj = convert(obj.getMeta(),resObj);
+			
+			Map<String,String> audioMap = obj.getAudio();
+			if (audioMap!=null && audioMap.size()>0){
+				resObj.put("audio",convert(audioMap,null));
+			}
+
+			Map<String,String> videoMap = obj.getVideo();
+			if (videoMap!=null && videoMap.size()>0){
+				resObj.put("video",convert(videoMap,null));
+			}
+
+			return resObj;
+		}
+		finally{
+			if (get!=null){
+				get.releaseConnection();
+			}
+		}
 	}
 }
