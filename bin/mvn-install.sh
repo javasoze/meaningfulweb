6a027917d0ea68606d3915374ff92249f4dfb928
John Wang <john.wang@gmail.com>
2011-02-23 15:45:16 -0800

    updated to tika 0.9

--- bin/mvn-install.sh
+++ bin/mvn-install.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-echo "installing tika core snapshot..."
-mvn install:install-file -Dfile=lib/tika-core-0.9-SNAPSHOT.jar -DgroupId=org.apache.tika -DartifactId=tika-core -Dversion=0.9-SNAPSHOT -Dpackaging=jar
-echo "installing tika parser snapshot..."
-mvn install:install-file -Dfile=lib/tika-parsers-0.9-SNAPSHOT.jar -DgroupId=org.apache.tika -DartifactId=tika-parsers -Dversion=0.9-SNAPSHOT -Dpackaging=jar
 echo "installing htmlcleaner 2.2"
 mvn install:install-file -Dfile=lib/htmlcleaner-2.2.jar -DgroupId=org.htmlcleaner -DartifactId=htmlcleaner -Dversion=2.2 -Dpackaging=jar
 echo "installing nekohtml 1.9.13"
