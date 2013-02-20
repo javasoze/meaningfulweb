87fecb3a2eaf4e773621c966d9d9943715b7ce02
John Wang <john.wang@gmail.com>
2013-02-19 20:31:00 -0800

    pom fixed, no need for mvn install script

--- bin/mvn-install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "installing htmlcleaner 2.2"
-mvn install:install-file -Dfile=lib/htmlcleaner-2.2.jar -DgroupId=org.htmlcleaner -DartifactId=htmlcleaner -Dversion=2.2 -Dpackaging=jar
-echo "installing nekohtml 1.9.13"
-mvn install:install-file -Dfile=lib/nekohtml-1.9.13.jar -DgroupId=nekohtml -DartifactId=nekohtml -Dversion=1.9.13 -Dpackaging=jar
